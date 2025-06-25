5a1fb33162be2f10ef4ab177f5d3eea8d380f520
Matthew Yglesias <mdywrk@gmail.com>
2025-06-24 20:58:14 -0400

    Completed Exercise 1.

--- SelectionStatementExercise/Program.cs
+++ SelectionStatementExercise/Program.cs
@@ -2,9 +2,43 @@
 {
     public class Program
     {
-        static void Main(string[] args)
+        
+        public static void NumberGuessingGameIntro()
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Let's play a guessing game!");
+            Console.WriteLine("I'm thinking of a number between 1 and 1000!");
+            Input();
+        }
+
+        public static void Input()
+        {   Console.WriteLine("Guess a number and I will tell you if you are too high, too low, or correct!");
+            var r = new Random();
+            var favNumber = r.Next(1,1000);
+            var userInput = int.Parse(Console.ReadLine());
+            if (userInput < favNumber)
+            {
+                Console.WriteLine("Too Low");
+            }
+            else if (userInput > favNumber)
+            {
+                Console.WriteLine("Too high!");
+            }
+            else if (userInput == favNumber)
+            {
+                Console.WriteLine("You guessed it!!!");
+            }
+
+
+            
+            
+        }
+        static void Main(string[] args)
+        { 
+        NumberGuessingGameIntro();
+            
+            
+            
+          
         }
     }
 }
